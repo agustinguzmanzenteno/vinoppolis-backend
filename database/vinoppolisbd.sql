fd885c6c5ac99ff00e92ecd9532174e71675ccbc
agustinguzmanzenteno <agustinguzmanzenteno@gmail.com>
2025-04-14 14:58:44 -0400

    Cambios en la tabla Productos

--- database/vinoppolisbd.sql
+++ database/vinoppolisbd.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS vinoppolisbd;
 USE vinoppolisbd;
 CREATE TABLE producto (
     id INT AUTO_INCREMENT PRIMARY KEY,
- 	codigoProducto VARCHAR(50) NOT NULL UNIQUE,
+ 	codigoProducto INT NOT NULL UNIQUE,
   	nombreProducto VARCHAR(250) NOT NULL,
- 	precio INT NOT NULL,
+ 	precio DOUBLE NOT NULL,
  	categoria VARCHAR(250),
  	stockActual INT DEFAULT 0,
  	imagen VARCHAR(250) 
